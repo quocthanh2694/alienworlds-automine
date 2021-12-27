cd24b6d9b17ce38f9cc0ad6aed71fe52e7e75033
quocthanh2694 <quocthanh2694@gmail.com>
2021-12-27 13:34:58 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         Alien world auto
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://play.alienworlds.io
+// @include        https://play.alienworlds.io
 // @icon         https://www.google.com/s2/favicons?domain=alienworlds.io
 // @grant        none
 // ==/UserScript==
