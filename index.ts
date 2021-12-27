69831d33a98e4c8ed692e95b0b79e76dbf57cdc5
quocthanh2694 <quocthanh2694@gmail.com>
2021-12-27 13:36:59 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @include        https://play.alienworlds.io
+// @include      https://play.alienworlds.io*
 // @icon         https://www.google.com/s2/favicons?domain=alienworlds.io
 // @grant        none
 // ==/UserScript==
